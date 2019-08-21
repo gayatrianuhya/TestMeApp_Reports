896ab5dfb22a88af537fb7a2232675b5133e4351
gayatrianuhya <54269301+gayatrianuhya@users.noreply.github.com>
2019-08-21 14:10:29 +0530

    Update Runnerclass_123.java

--- src/test/java/cucumbertesting123/Runnerclass_123.java
+++ src/test/java/cucumbertesting123/Runnerclass_123.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class )
-@CucumberOptions(features="Feature456",glue="cucumbertesting123",plugin={"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(features="Feature456",glue="cucumbertesting123",plugin={"json:target/cucumber.json"})
 
 
 public class Runnerclass_123 {
